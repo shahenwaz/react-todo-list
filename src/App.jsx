df5539e8c1e5b06fd2e37e26fd1672287d3aa964
Shahenwaz Muzahid <kmuzahid52@gmail.com>
2024-06-07 17:45:50 +0100

    Added a static form

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,34 @@
 import "./style.css";
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+
+      <h1 className="header">TODO List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
